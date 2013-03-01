61ed66caa2ad9aeff4f605d651579b569572f342
Adrian Collado <acollado@citlink.net>
2013-03-01 13:35:26 -0500

    Updated Bootkit Drivers
    Added PIC driver and updated FDC

--- src/bootkit/include/driver/PIC.h
+++ src/bootkit/include/driver/PIC.h
@@ -24,75 +24,21 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
  ****************************************************************************************
- * Date: 2/27/2013 - 2/27/2013
+ * Date of creation:  3/1/2013
+ * Date of last edit: 3/1/2013
  ****************************************************************************************
- * FDC.h - Generic Driver Interface
+ * PIC.h - Programmable Interrupt Controller Header
  ****************************************************************************************
  */
  
-#ifndef  _BOOTKIT_FDC_H_
-#define  _BOOTKIT_FDC_H_
-
-typedef enum FDCDORBitflags {
-	MOTOR_D = 0x80,
-	MOTOR_C = 0x40,
-	MOTOR_B = 0x20,
-	MOTOR_A = 0x10,
-	IRQ     = 0x08,
-	RESET   = 0x04,
-	DSEL1   = 0x02,
-	DSEL0   = 0x01
-};
-
-typedef enum FDCMSRBitflags {
-	RQM     = 0x80,
-	DIO     = 0x40,
-	NDMA    = 0x20,
-	CB      = 0x10,
-	ACTD    = 0x08,
-	ACTC    = 0x04,
-	ACTB    = 0x02,
-	ACTA    = 0x01
-};
-
-typedef enum FDCRegisters {
-	REG_STATUS_A                = 0x3F0, // Read
-	REG_STATUS_B                = 0x3F1, // Read
-	REG_DIGITAL_OUTPUT          = 0x3F2,
-	REG_TAPE_DRIVE              = 0x3F3,
-	REG_MAIN_STATUS             = 0x3F4, // Read
-	REG_DATARATE_SELECT         = 0x3F4, // Write
-	REG_FIFO                    = 0x3F5,
-	REG_DIGITAL_INPUT           = 0x3F7, // Read
-	REG_CONFIGURATION_CONTROL   = 0x3F7	 // Write
-};
-
-typedef enum FDCCommands {
-	READ_TRACK                  = 0x02,
-	SPECIFY                     = 0x03,
-	SENSE_DRIVE_STATUS          = 0x04,
-	WRITE_DATA                  = 0x05,
-	READ_DATA                   = 0x06,
-	RECALIBRATE                 = 0x07,
-	SENSE_INTERRUPT             = 0x08,
-	WRITE_DELETED_DATA          = 0x09,
-	READ_ID                     = 0x0A,
-	READ_DELETED_DATA           = 0x0C,
-	FORMAT_TRACK                = 0x0D,
-	SEEK                        = 0x0F,
-	VERSION                     = 0x10,
-	SCAN_EQUAL                  = 0x11,
-	PERPENDICULAR_MODE          = 0x12,
-	CONFIGURE                   = 0x13,
-	LOCK                        = 0x14,
-	VERIFY                      = 0x16,
-	SCAN_LOW_OR_EQUAL           = 0x19,
-	SCAN_HIGH_OR_EQUAL          = 0x1D
-};
-
-/****************************
- * Functions
- ****************************
- */
-
-#endif //_BOOTKIT_FDC_H_
+ // Port Locations - Master
+ #define PIC_MASTER_BASE    0x20
+ #define PIC_MASTER_COMMAND 0x20
+ #define PIC_MASTER_DATA    0x21
+ 
+ // Port Locations - Slave
+ #define PIC_SLAVE_BASE     0xA0
+ #define PIC_SLAVE_COMMAND  0xA0
+ #define PIC_SLAVE_DATA     0xA1
+ 
+ 
